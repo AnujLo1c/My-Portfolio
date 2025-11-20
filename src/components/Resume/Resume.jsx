d8b008c517694d5ea77232995fbef7096d0677d5
Anuj Lowanshi <anujlowanshi3@gmail.com>
2025-11-20 13:25:33 +0530

    feat: mobile home done , desktop view completed

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -9,25 +9,55 @@ export default function Resume() {
       <h2 className="resume-title">
         Resume
         <span className="underline"></span>
-      </h2>
+      </h2> 
 
       {/* Buttons */}
       <div className="resume-buttons">
         <a
           className="btn primary"
-          href="/Anuj_Lowanshi_Resume.pdf"
+          href="/My-Portfolio/public/Anuj_Lowanshi_Resume.pdf"
           target="_blank"
           rel="noopener noreferrer"
         >
           View Resume
+            <svg 
+      className="icon" 
+      width="18" 
+      height="18" 
+      viewBox="0 0 24 24" 
+      fill="none" 
+      stroke="currentColor" 
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M18 13v6a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+      <polyline points="15 3 21 3 21 9" />
+      <line x1="10" y1="14" x2="21" y2="3" />
+    </svg>
         </a>
 
         <a
           className="btn"
-          href="/Anuj_Lowanshi_Resume.pdf"
+          href="/My-Portfolio/public/Anuj_Lowanshi_Resume.pdf"
           download
         >
           Download Resume
+          <svg 
+      className="icon" 
+      width="18" 
+      height="18" 
+      viewBox="0 0 24 24" 
+      fill="none" 
+      stroke="currentColor" 
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M12 5v14" />
+      <polyline points="5 12 12 19 19 12" />
+      <rect x="5" y="19" width="14" height="2" />
+    </svg>
         </a>
       </div>
 
